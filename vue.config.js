0fc80a81d043362e7d55818962c21cc90fff9de1
Yu-Rong <gcobc50335@gmail.com>
2020-12-27 01:29:28 +0800

    outputDir 測試

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "lunchSavior" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "lunchSavior" : "./",
+  outputDir: "dist",
 };
